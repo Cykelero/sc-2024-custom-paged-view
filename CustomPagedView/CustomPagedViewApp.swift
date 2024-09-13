dad4e017f120fc5e74eb765745663413245e80cf
Nathan Manceaux-Panot <nathan@lemon.garden>
2024-09-13 16:04:04 +0200

    Initial Commit

---
+++ CustomPagedView/CustomPagedViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomPagedViewApp.swift
+//  CustomPagedView
+//
+//  Created by Nathan Manceaux-Panot on 2024-09-13.
+//
+
+import SwiftUI
+
+@main
+struct CustomPagedViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
